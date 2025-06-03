29e4effb185ed1ef8431970e149837678cdbb903
DanielPortillo26 <danielportilloalejo@gmail.com>
2025-06-03 21:21:53 +0200

    Contact form validation added (basic)

--- js/main.js
+++ js/main.js
@@ -4,7 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (form) {
     form.addEventListener("submit", function (e) {
-      
+      e.preventDefault();
+
+      const name = form.name.value.trim();
+      const email = form.email.value.trim();
+      const msg = form.message.value.trim();
+
+      if (!name || !email || !msg) {
+        message.textContent = "Please fill out all fields to proceed.";
+        return;
+      }
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Characters to be checked
+      if (!emailRegex.test(email)) {
+        message.textContent = "Please enter a valid email.";
+        return; // Exit sub if no valif address
+      }
+
+      // Temporary submission message. This I edit later with the page reload
+      message.style.color = "green";
+      message.textContent = "We appreciate your message! It has been sent.";
 
       form.reset();
     });
