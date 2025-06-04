100d7c4216b4f32a52a949cc061e8f25cbe9abf2
DanielPortillo26 <danielportilloalejo@gmail.com>
2025-06-04 11:17:49 +0200

    Started with chatbot logic in js

---
+++ js/chatbot.js
@@ -0,0 +1,13 @@
+// DOMContentLoaded block - 04.06.2025
+document.addEventListener("DOMContentLoaded", () => {
+  const chatForm = document.getElementById("chatForm");
+  const chatInput = document.getElementById("chatInput");
+  const chatMessages = document.getElementById("chatMessages");
+
+  const API_KEY = "API_KEY"; // 
+
+  if (!chatForm || !chatInput || !chatMessages) return;
+
+  
+
+});
