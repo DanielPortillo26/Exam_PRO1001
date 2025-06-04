3058327d3b456f8f8a8137b86bbe74850c2172c2
DanielPortillo26 <danielportilloalejo@gmail.com>
2025-06-04 11:38:18 +0200

    Added some functions needed for my logic in the chatbot js

--- js/chatbot.js
+++ js/chatbot.js
@@ -4,10 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const chatInput = document.getElementById("chatInput");
   const chatMessages = document.getElementById("chatMessages");
 
-  const API_KEY = "API_KEY"; // 
+  const API_KEY = "API_KEY"; // check this later
 
+  // Safe chack: if any of the following are missing then exit the method, to avoid errors
   if (!chatForm || !chatInput || !chatMessages) return;
 
   
+  // Helper funcitions - 04.06.2025
+  function appendMessage(sender, text) {
+    const msg = document.createElement("p");
+    msg.innerHTML = `<strong>${sender}:</strong> ${text}`;
+    chatMessages.appendChild(msg);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+
+  function replaceLastMessage(sender, text) {
+    const last = chatMessages.lastElementChild;
+    if (last) {
+      last.innerHTML = `<strong>${sender}:</strong> ${text}`;
+    }
+  }
+
 
 });
